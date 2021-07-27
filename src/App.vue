eb526e7a202b949e0d5913c8076815fa595f28e0
Omar Hernandez <omar_alberto_h@yahoo.es>
2021-07-27 11:50:24 -0500

    Adding Router

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,30 @@
 <template>
-<div>
-  <headInfo></headInfo>
-  <Header></Header>
-  <Slider></Slider>
-  <CardA></CardA>
-  <SliderA></SliderA>
-  <CardB></CardB>
-  <Footer></Footer>
-
-</div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script>
-import HeadInfo from './components/HeadInfo.vue'//
-import Header from './components/Header.vue'
-import Slider from './components/Slider.vue'
-import CardA from './components/CardA.vue'
-import SliderA from './components/SliderA.vue'
-import CardB from './components/CardB.vue'
-import Footer from './components/Footer.vue'
-
-export default {
-  name: 'App',
-  components: { 
-    HeadInfo,
-    Header,
-    Slider,
-    CardA,
-    SliderA,
-    CardB,
-    Footer,
-  }
-}
-</script>
-
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
