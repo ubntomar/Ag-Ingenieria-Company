e24d5928ad6df5ccfbf6a24092c4bef66f49b3fb
Omar Hernandez <omar_alberto_h@yahoo.es>
2021-08-26 22:58:42 -0500

    server listen

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const mongoose= require("mongoose")  
+
+// const app= express()
+
+// const authRoutes=require("./routes/auth")
+
+// app.use("/api/user",authRoutes)
+// app.listen(3000,()=> console.log("server is runing!"))
---
+++ backend/routes/auth.js
@@ -0,0 +1,6 @@
+const router= require("express").Router()
+router.post("/register",(req,res)=>{
+    res.send("Register area!")
+})
+
+module.exports= router
